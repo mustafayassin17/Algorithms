6aa6c58ead6472a60f6fe168f97da66ef1da3cdb
mustafayassin17 <yassinmustafa949@gmail.com>
2023-03-23 13:51:23 +0200

    No Comments

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int binary_search(int list[], int item, int low, int high);
+
+int main() {
+    int my_list[] = {1, 3, 5, 7, 9};
+    int low = 0;
+    int high = sizeof(my_list) / sizeof(my_list[0]);
+    int num = 9;
+    int search_value = binary_search(my_list, num, low, high);
+    if (search_value == 100) {
+        printf("Not found");
+    } else {
+        printf("Element is found at index %d", search_value);
+    }
+    return 0;
+}
+
+int binary_search(int list[], int item, int low, int high) {
+    while (low <= high) {
+        int mid = (high + low) / 2;
+        int guess = list[mid];
+        if (guess == item) {
+            return mid;
+        }
+        if (guess > item) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return 100;
+}
